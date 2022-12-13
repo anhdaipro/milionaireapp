be129fce1caeb6027e4015048ac805737c9d52f4
phamdai <daipham952@gmail.com>
2022-12-14 06:20:44 +0700

    commit

--- milionaire/settings.py
+++ milionaire/settings.py
@@ -98,7 +98,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=24),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=2),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=4),
     'ROTATE_REFRESH_TOKENS': False,
     'BLACKLIST_AFTER_ROTATION': False,
