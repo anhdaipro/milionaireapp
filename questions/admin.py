ef6736b36ea8c1d9e44919420135d5eecc98b49e
phamdai <daipham952@gmail.com>
2022-12-14 06:27:39 +0700

    commit

--- questions/admin.py
+++ questions/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
 from .models import *
 # Register your models here.
-admin.site.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ['question','answer','level']
+
+admin.site.register(Question,QuestionAdmin)
 admin.site.register(QuestionUser)
 admin.site.register(AnswerUser)
